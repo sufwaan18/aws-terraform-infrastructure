c77f72a372900240a38b8e37131fff80ec014534
Sufwaan-Amees <151483283+sufwaan18@users.noreply.github.com>
2026-06-17 18:36:35 -0500

    Add Terraform AWS provider and VPC resource

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "cloud-admin-vpc"
+  }
+}
